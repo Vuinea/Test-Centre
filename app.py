f135d671a16c1fcbe978d4b096fa5217a532f663
Vuinea <vinodh.gnanakkan@gmail.com>
2025-04-02 13:01:27 -0400

    made db name above app config

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
 DB_NAME = 'test_centre.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = f"sqlite:///{DB_NAME}"
 db = SQLAlchemy(app)
