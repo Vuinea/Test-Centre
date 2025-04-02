c576ed5853c7705b64b17913be92e3c64284b3dd
Vuinea <vinodh.gnanakkan@gmail.com>
2025-04-01 20:26:41 -0400

    from backend

--- __init__.py
+++ __init__.py
@@ -1,9 +1,13 @@
-from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
+from flask import render_template
+from .app import app, db, DB_NAME
+from .db.models import *
+import os
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///test_centre.db"
-db = SQLAlchemy(app)
+db_dir = os.path.join(os.getcwd(), 'instance/test_centre.db')
+
+with app.app_context():
+    if not os.path.exists(db_dir):
+        db.create_all()
 
 
 @app.route('/')
@@ -13,3 +17,9 @@ def home():
 @app.route('/test')
 def test():
     return render_template('teacher/test_form.html')
+
+from .blueprints.teacher import teacher
+app.register_blueprint(teacher)
+
+# flask --app Test-Centre --debug  run
+
