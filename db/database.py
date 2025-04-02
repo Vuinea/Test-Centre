e5a9c4ddf6415475041939f1aaa795025c240a97
Vuinea <vinodh.gnanakkan@gmail.com>
2025-04-01 22:47:07 -0400

    Automatically feeds data from the excel spreadsheet to the database

--- db/database.py
+++ db/database.py
@@ -1,43 +1,6 @@
-import sqlite3
+import os
 import pandas as pd
-from .models import db
-
-def create_database():
-    pass
-    # conn = sqlite3.connect(db_name)
-    # cursor = conn.cursor()
-    # # table creating the student
-    # cursor.execute("""
-    #     CREATE TABLE IF NOT EXISTS Students (
-    #         id INTEGER PRIMARY KEY AUTOINCREMENT,
-    #         first_name TEXT,
-    #         preferred TEXT,
-    #         surname TEXT,
-    #         TAG_teacher TEXT,
-    #         block_1 TEXT,
-    #         block_2 TEXT,
-    #         block_3 TEXT,
-    #         block_4 TEXT,
-    #         block_5 TEXT,
-    #         block_6 TEXT,
-    #         block_7 TEXT,
-    #         block_8 TEXT,
-    #         diagnosis TEXT,
-    #         extra_time TEXT,
-    #         room TEXT,
-    #         computer TEXT,
-    #         AP_additional_support TEXT,
-    #         AP_additional_strategies TEXT,
-    #         overview TEXT,
-    #         other_concerns TEXT,
-    #         ELL_extra_time TEXT,
-    #         ELL_IB_approved TEXT,
-    #         dictionary TEXT,
-    #         ELL_additional_notes TEXT
-    #     )
-    # """)
-    # conn.commit()
-    # conn.close()
+from .models import db, Student
 
 def load_data_from_excel(file_path):
     xl = pd.ExcelFile(file_path)
@@ -81,17 +44,10 @@ def load_data_from_excel(file_path):
                       })
     return students_df, ap_df, ell_df
 
-#i merged all the data tabs into one list if names matched, so the database doesnt have different tabs
+# I merged all the data tabs into one list if names matched, so the database doesn't have different tabs
 
 def merge_data(students_df: pd.DataFrame, ap_df: pd.DataFrame, ell_df: pd.DataFrame):
     merged_df = students_df.copy()
-    # print(students_df['First Name'].dtype)
-    # print(ap_df['First Name'].dtype)
-    # print(ell_df['First Name'].dtype)
-    # print(students_df['Preferred'].dtype)
-    # print(ap_df['Preferred'].dtype)
-    # print(ell_df['Preferred'].dtype)
-    #ap data merge
     ap_df.drop_duplicates(subset=["First Name", "Surname"], keep='first', inplace=True)
     merged_df = pd.merge(merged_df, ap_df, on=["First Name", "Preferred", "Surname"], how='left')
 
@@ -102,35 +58,40 @@ def merge_data(students_df: pd.DataFrame, ap_df: pd.DataFrame, ell_df: pd.DataFr
     merged_df.fillna("", inplace=True)
     return merged_df
 
-def insert_into_database(merged_df: pd.DataFrame):
-    pass
+def insert_into_database():
+    data_path = os.path.join(os.getcwd(), 'instance/ap_ell_master_for_teachers.xlsx')
+    data = load_data_from_excel(data_path)
+    merged = merge_data(*data)
 
+    for _, row in merged.iterrows():
+        student_dict = {
+            'first_name': row['First Name'],
+            'preferred': row['Preferred'],
+            'surname': row['Surname'],
+            'TAG_teacher': row['TAG Teacher'],
+            'block_1': row['Block 1'],
+            'block_2': row['Block 2'],
+            'block_3': row['Block 3'],
+            'block_4': row['Block 4'],
+            'block_5': row['Block 5'],
+            'block_6': row['Block 6'],
+            'block_7': row['Block 7'],
+            'block_8': row['Block 8'],
+            'diagnosis': row['Diagnosis'],
+            'extra_time': row['Extra Time_x'],
+            'room': row['Room?'],
+            'computer': row['Computer?'],
+            'AP_additional_support': row['AP Additional Support (Assessment Accomodation)'],
+            'AP_additional_strategies': row['AP Additional Strategies'],
+            'overview': row['Overview'],
+            'other_concerns': row['Other Concerns'],
+            'ELL_extra_time': row['Extra Time_y'],
+            'ELL_IB_approved': row['ELL IB Approved?'],
+            'dictionary': row['Dictionary?'],
+            'ELL_additional_notes': row['ELL Additional Notes'],
+        }
+        student = Student(**student_dict)
+        db.session.add(student)
+    db.session.commit()
+    print('Successfully added to db!')
 
-    # conn = sqlite3.connect(db_name)
-    # cursor = conn.cursor()
-
-    # for _, row in merged_df.iterrows():
-    #     cursor.execute("""
-    #         INSERT INTO Students (
-    #             first_name, preferred, surname, TAG_teacher, block_1, block_2, block_3, block_4, block_5, block_6, block_7, block_8,
-    #             diagnosis, extra_time, room, computer, AP_additional_support, AP_additional_strategies, overview, other_concerns,
-    #             ELL_extra_time, ELL_IB_approved, dictionary, ELL_additional_notes
-    #         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-    #     """, tuple(row))
-
-    # conn.commit()
-    # conn.close()
-
-def main():
-    db_name = "students.db"
-    excel_file = "students_data.xlsx"
-
-    create_database(db_name)
-    students_df, ap_df, ell_df = load_data_from_excel(excel_file)
-    merged_df = merge_data(students_df, ap_df, ell_df)
-    insert_into_database(merged_df, db_name)
-
-    print("Database has been successfully created and populated.")
-
-if __name__ == "__main__":
-    main()
