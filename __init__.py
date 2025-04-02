a3dab2e653b89b67fb46d7b02e81b732083bf18c
Vuinea <vinodh.gnanakkan@gmail.com>
2025-04-01 22:41:40 -0400

    made basic setup for inserting intno database (have to move into functions)

--- __init__.py
+++ __init__.py
@@ -1,13 +1,54 @@
 from flask import render_template
 from .app import app, db, DB_NAME
 from .db.models import *
+from .db.database import load_data_from_excel, merge_data
 import os
 
 db_dir = os.path.join(os.getcwd(), 'instance/test_centre.db')
 
 with app.app_context():
     if not os.path.exists(db_dir):
-        db.create_all()
+        db.drop_all()
+    
+    db.create_all()
+    data = load_data_from_excel('instance/ap_ell_master_for_teachers.xlsx')
+    merged = merge_data(*data)
+    records = merged.to_dict(orient="records")
+    # students = [Student(**record) for record in records]
+    # db.session.bulk_save_objects(students)
+    # db.session.commit()
+
+    for _, row in merged.iterrows():
+        student_dict = {
+            'first_name': row['First Name'],
+            'preferred': row['Preferred'],
+            'surname': row['Surname'],
+            'TAG_teacher': row['TAG Teacher'],
+            'block_1': row['Block 1'],
+            'block_2': row['Block 2'],
+            'block_3': row['Block 3'],
+            'block_4': row['Block 4'],
+            'block_5': row['Block 5'],
+            'block_6': row['Block 6'],
+            'block_7': row['Block 7'],
+            'block_8': row['Block 8'],
+            'diagnosis': row['Diagnosis'],
+            'extra_time': row['Extra Time_x'],
+            'room': row['Room?'],
+            'computer': row['Computer?'],
+            'AP_additional_support': row['AP Additional Support (Assessment Accomodation)'],
+            'AP_additional_strategies': row['AP Additional Strategies'],
+            'overview': row['Overview'],
+            'other_concerns': row['Other Concerns'],
+            'ELL_extra_time': row['Extra Time_y'],
+            'ELL_IB_approved': row['ELL IB Approved?'],
+            'dictionary': row['Dictionary?'],
+            'ELL_additional_notes': row['ELL Additional Notes'],
+        }
+        student = Student(**student_dict)
+        db.session.add(student)
+    db.session.commit()
+    print('Successfully added to db!')
 
 
 @app.route('/')
--- db/database.py
+++ db/database.py
@@ -41,42 +41,85 @@ def create_database():
 
 def load_data_from_excel(file_path):
     xl = pd.ExcelFile(file_path)
-    students_df = xl.parse("Students")
-    ap_df = xl.parse("AP")
-    ell_df = xl.parse("ELL")
+    students_df = xl.parse("Students",
+                           converters={
+                                        "First Name": str, 
+                                        "Preferred": str, 
+                                       "Surname": str,
+                                        'TAG teacher': str,
+                                       'Block 1': str,
+                                       'Block 2': str,
+                                       'Block 3': str,
+                                        'Block 4': str,
+                                        'Block 5': str,
+                                        'Block 6': str,
+                                        'Block 7': str,
+                                        'Block 8': str,
+                                    })
+    ap_df = xl.parse("AP", 
+                     converters={
+                        'First Name': str, 
+                        'Preferred': str, 
+                        'Surname': str,
+                        'Diagnosis': str,
+                        'Extra Time': str,
+                        'Room': str,
+                        'Computer': str,
+                        'AP Additional Support (Assessment Accomodation)': str,
+                        'AP Additional Strategies': str,
+                        'Overview': str,
+                        'Other Concerns': str
+                    })
+    ell_df = xl.parse("ELL", 
+                      converters={
+                        'First Name': str, 
+                        'Surname': str,
+                        'Extra Time': str,
+                        'ELL IB Approved': str,
+                        'Dictionary': str,
+                        'ELL Additional Notes': str
+                      })
     return students_df, ap_df, ell_df
 
 #i merged all the data tabs into one list if names matched, so the database doesnt have different tabs
 
-def merge_data(students_df, ap_df, ell_df):
+def merge_data(students_df: pd.DataFrame, ap_df: pd.DataFrame, ell_df: pd.DataFrame):
     merged_df = students_df.copy()
-
+    # print(students_df['First Name'].dtype)
+    # print(ap_df['First Name'].dtype)
+    # print(ell_df['First Name'].dtype)
+    # print(students_df['Preferred'].dtype)
+    # print(ap_df['Preferred'].dtype)
+    # print(ell_df['Preferred'].dtype)
     #ap data merge
-    ap_df.drop_duplicates(subset=["first name", "surname"], keep='first', inplace=True)
-    merged_df = pd.merge(merged_df, ap_df, on=["first name", "preferred", "surname"], how='left')
+    ap_df.drop_duplicates(subset=["First Name", "Surname"], keep='first', inplace=True)
+    merged_df = pd.merge(merged_df, ap_df, on=["First Name", "Preferred", "Surname"], how='left')
 
     #ell data merge
     ell_df.drop_duplicates(subset=["first name", "surname"], keep='first', inplace=True)
-    merged_df = pd.merge(merged_df, ell_df, on=["first name", "preferred", "surname"], how='left')
+    merged_df = pd.merge(merged_df, ell_df, on=["First Name", "Preferred", "Surname"], how='left')
 
     merged_df.fillna("", inplace=True)
     return merged_df
 
-def insert_into_database(merged_df, db_name="students.db"):
-    conn = sqlite3.connect(db_name)
-    cursor = conn.cursor()
+def insert_into_database(merged_df: pd.DataFrame):
+    pass
 
-    for _, row in merged_df.iterrows():
-        cursor.execute("""
-            INSERT INTO Students (
-                first_name, preferred, surname, TAG_teacher, block_1, block_2, block_3, block_4, block_5, block_6, block_7, block_8,
-                diagnosis, extra_time, room, computer, AP_additional_support, AP_additional_strategies, overview, other_concerns,
-                ELL_extra_time, ELL_IB_approved, dictionary, ELL_additional_notes
-            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
-        """, tuple(row))
 
-    conn.commit()
-    conn.close()
+    # conn = sqlite3.connect(db_name)
+    # cursor = conn.cursor()
+
+    # for _, row in merged_df.iterrows():
+    #     cursor.execute("""
+    #         INSERT INTO Students (
+    #             first_name, preferred, surname, TAG_teacher, block_1, block_2, block_3, block_4, block_5, block_6, block_7, block_8,
+    #             diagnosis, extra_time, room, computer, AP_additional_support, AP_additional_strategies, overview, other_concerns,
+    #             ELL_extra_time, ELL_IB_approved, dictionary, ELL_additional_notes
+    #         ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
+    #     """, tuple(row))
+
+    # conn.commit()
+    # conn.close()
 
 def main():
     db_name = "students.db"
