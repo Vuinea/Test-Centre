707976f61b0aec00afdacba2ad6c6894aca0e679
Vuinea <vinodh.gnanakkan@gmail.com>
2025-04-02 21:42:57 -0400

    created the read for tests

--- blueprints/teacher.py
+++ blueprints/teacher.py
@@ -1,15 +1,16 @@
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, redirect, url_for
 from ..forms.create_test import CreateTestForm
 from ..db.models import Test, Teacher
-from ..app import db
+from ..app import app, db
 
-bp = Blueprint('teacher', __name__)
+bp = Blueprint('teacher', __name__, url_prefix='/tests')
+with app.app_context():
+    TEACHER = db.session.query(Teacher).where(Teacher.id == 1).first()
 
 
 @bp.route('/create_test', methods=['GET', 'POST'])
 def create_test():
     form = CreateTestForm()
-    teacher = db.session.query(Teacher).where(Teacher.id == 1).first()
     if request.method == 'POST' and form.validate_on_submit():
         # Extract form data
         name = form.name.data
@@ -17,8 +18,13 @@ def create_test():
         open_note = form.open_note.data
         comments = form.comments.data
         # Create and save the Test object
-        test = Test(name=name, time=time, open_note=open_note, comments=comments, teacher=teacher)
+        test = Test(name=name, time=time, open_note=open_note, comments=comments, teacher=TEACHER)
         db.session.add(test)
         db.session.commit()
     return render_template('teacher/create_test_form.html', form=form)
+
+@bp.route('/')
+def get_tests():
+    tests = db.session.query(Test).filter(Test.teacher == TEACHER).all()
+    return render_template('teacher/tests.html', tests=tests)
 
